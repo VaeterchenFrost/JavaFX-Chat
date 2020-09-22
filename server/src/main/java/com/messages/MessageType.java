f06bf5b1775dadc84daddbf8cd57760149e24c99
VaeterchenFrost <martin.roebke@web.de>
2020-09-22 18:10:32 +0200

    format docstring author

--- server/src/main/java/com/messages/MessageType.java
+++ server/src/main/java/com/messages/MessageType.java
@@ -2,9 +2,11 @@ package com.messages;
 
 /**
  * @author Dominic
+ *         <p>
+ *         Website: www.dominicheal.com
+ *         <p>
+ *         Github: www.github.com/DomHeal
  * @since 03-Sep-16
- * Website: www.dominicheal.com
- * Github: www.github.com/DomHeal
  */
 public enum MessageType {
     DISCONNECTED, CONNECTED, STATUS, USER, SERVER, NOTIFICATION, VOICE
