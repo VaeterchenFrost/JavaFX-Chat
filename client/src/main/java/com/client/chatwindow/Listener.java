35c45c508924bad6641ef6d35cf2c9f278c2fcf8
Martin Röbke <martin.roebke@web.de>
2020-09-22 23:08:19 +0200

    Fix Res Leaks in try
    and remove intermed variables, using resSocket directly

--- client/src/main/java/com/client/chatwindow/Listener.java
+++ client/src/main/java/com/client/chatwindow/Listener.java
@@ -24,9 +24,7 @@ public class Listener implements Runnable {
     private static String picture;
     private static ObjectOutputStream oos;
     private Socket socket;
-    private InputStream is;
     private ObjectInputStream input;
-    private OutputStream outputStream;
 
     public Listener(String hostname, int port, String username, String picture, ChatController controller) {
         this.hostname = hostname;
@@ -37,13 +35,13 @@ public class Listener implements Runnable {
     }
 
     public void run() {
-        try {
-            socket = new Socket(hostname, port);
+        try (Socket resSocket = new Socket(hostname, port);
+                ObjectOutputStream resOOS = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream rIS = new ObjectInputStream(socket.getInputStream())) {
+            socket = resSocket;
+            input = rIS;
+            oos = resOOS;
             LoginController.getInstance().showScene();
-            outputStream = socket.getOutputStream();
-            oos = new ObjectOutputStream(outputStream);
-            is = socket.getInputStream();
-            input = new ObjectInputStream(is);
         } catch (IOException e) {
             LoginController.getInstance().showErrorDialog("Could not connect to server");
             logger.error("Could not Connect");
