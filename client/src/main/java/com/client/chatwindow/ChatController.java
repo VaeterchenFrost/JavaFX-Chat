ffd4e6e10c4372f978897b50eff85a64de34a9f3
VaeterchenFrost <martin.roebke@web.de>
2020-09-22 15:53:02 +0200

    Format ChatController

--- client/src/main/java/com/client/chatwindow/ChatController.java
+++ client/src/main/java/com/client/chatwindow/ChatController.java
@@ -14,7 +14,6 @@ import com.traynotifications.animations.AnimationType;
 import com.traynotifications.notification.TrayNotification;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -44,27 +43,37 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
 public class ChatController implements Initializable {
 
-    @FXML private TextArea messageBox;
-    @FXML private Label usernameLabel;
-    @FXML private Label onlineCountLabel;
-    @FXML private ListView<User> userList;
-    @FXML private ImageView userImageView;
-    @FXML private Button recordBtn;
-    @FXML ListView<HBox> chatPane;
-    @FXML BorderPane borderPane;
-    @FXML ComboBox<String> statusComboBox;
-    @FXML ImageView microphoneImageView;
+    @FXML
+    private TextArea messageBox;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label onlineCountLabel;
+    @FXML
+    private ListView<User> userList;
+    @FXML
+    private ImageView userImageView;
+    @FXML
+    private Button recordBtn;
+    @FXML
+    ListView<HBox> chatPane;
+    @FXML
+    BorderPane borderPane;
+    @FXML
+    ComboBox<String> statusComboBox;
+    @FXML
+    ImageView microphoneImageView;
 
-    Image microphoneActiveImage = new Image(getClass().getClassLoader().getResource("images/microphone-active.png").toString());
-    Image microphoneInactiveImage = new Image(getClass().getClassLoader().getResource("images/microphone.png").toString());
+    Image microphoneActiveImage = new Image(
+            getClass().getClassLoader().getResource("images/microphone-active.png").toString());
+    Image microphoneInactiveImage = new Image(
+            getClass().getClassLoader().getResource("images/microphone.png").toString());
 
     private double xOffset;
     private double yOffset;
     Logger logger = LoggerFactory.getLogger(ChatController.class);
-
 
     public void sendButtonAction() throws IOException {
         String msg = messageBox.getText();
@@ -78,43 +87,42 @@ public class ChatController implements Initializable {
         if (VoiceUtil.isRecording()) {
             Platform.runLater(() -> {
                 microphoneImageView.setImage(microphoneInactiveImage);
-                    }
-            );
+            });
             VoiceUtil.setRecording(false);
         } else {
             Platform.runLater(() -> {
                 microphoneImageView.setImage(microphoneActiveImage);
 
-                    }
-            );
+            });
             VoiceRecorder.captureAudio();
         }
     }
-
 
     public synchronized void addToChat(Message msg) {
         Task<HBox> othersMessages = new Task<HBox>() {
             @Override
             public HBox call() throws Exception {
-                Image image = new Image(getClass().getClassLoader().getResource("images/" + msg.getPicture().toLowerCase() + ".png").toString());
+                Image image = new Image(getClass().getClassLoader()
+                        .getResource("images/" + msg.getPicture().toLowerCase() + ".png").toString());
                 ImageView profileImage = new ImageView(image);
                 profileImage.setFitHeight(32);
                 profileImage.setFitWidth(32);
                 BubbledLabel bl6 = new BubbledLabel();
-                if (msg.getType() == MessageType.VOICE){
-                    ImageView imageview = new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString()));
+                if (msg.getType() == MessageType.VOICE) {
+                    ImageView imageview = new ImageView(
+                            new Image(getClass().getClassLoader().getResource("images/sound.png").toString()));
                     bl6.setGraphic(imageview);
                     bl6.setText("Sent a voice message!");
                     VoicePlayback.playAudio(msg.getVoiceMsg());
-                }else {
+                } else {
                     bl6.setText(msg.getName() + ": " + msg.getMsg());
                 }
-                bl6.setBackground(new Background(new BackgroundFill(Color.WHITE,null, null)));
+                bl6.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
                 HBox x = new HBox();
                 bl6.setBubbleSpec(BubbleSpec.FACE_LEFT_CENTER);
                 x.getChildren().addAll(profileImage, bl6);
-                if(logger.isDebugEnabled())
-                    logger.debug("ONLINE USERS: {}" , Integer.toString(msg.getUserlist().size()));
+                if (logger.isDebugEnabled())
+                    logger.debug("ONLINE USERS: {}", Integer.toString(msg.getUserlist().size()));
                 setOnlineLabel(Integer.toString(msg.getOnlineCount()));
                 return x;
             }
@@ -133,15 +141,15 @@ public class ChatController implements Initializable {
                 profileImage.setFitWidth(32);
 
                 BubbledLabel bl6 = new BubbledLabel();
-                if (msg.getType() == MessageType.VOICE){
-                    bl6.setGraphic(new ImageView(new Image(getClass().getClassLoader().getResource("images/sound.png").toString())));
+                if (msg.getType() == MessageType.VOICE) {
+                    bl6.setGraphic(new ImageView(
+                            new Image(getClass().getClassLoader().getResource("images/sound.png").toString())));
                     bl6.setText("Sent a voice message!");
                     VoicePlayback.playAudio(msg.getVoiceMsg());
-                }else {
+                } else {
                     bl6.setText(msg.getMsg());
                 }
-                bl6.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN,
-                        null, null)));
+                bl6.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, null, null)));
                 HBox x = new HBox();
                 x.setMaxWidth(chatPane.getWidth() - 20);
                 x.setAlignment(Pos.TOP_RIGHT);
@@ -164,12 +172,14 @@ public class ChatController implements Initializable {
             t.start();
         }
     }
+
     public void setUsernameLabel(String username) {
         this.usernameLabel.setText(username);
     }
 
     public void setImageLabel() throws IOException {
-        this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
+        this.userImageView
+                .setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
     }
 
     public void setOnlineLabel(String usercount) {
@@ -190,7 +200,8 @@ public class ChatController implements Initializable {
     /* Displays Notification when a user joins */
     public void newUserNotification(Message msg) {
         Platform.runLater(() -> {
-            Image profileImg = new Image(getClass().getClassLoader().getResource("images/" + msg.getPicture().toLowerCase() +".png").toString(),50,50,false,false);
+            Image profileImg = new Image(getClass().getClassLoader()
+                    .getResource("images/" + msg.getPicture().toLowerCase() + ".png").toString(), 50, 50, false, false);
             TrayNotification tray = new TrayNotification();
             tray.setTitle("A new user has joined!");
             tray.setMessage(msg.getName() + " has joined the JavaFX Chatroom!");
@@ -228,8 +239,7 @@ public class ChatController implements Initializable {
             public HBox call() throws Exception {
                 BubbledLabel bl6 = new BubbledLabel();
                 bl6.setText(msg.getMsg());
-                bl6.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE,
-                        null, null)));
+                bl6.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
                 HBox x = new HBox();
                 bl6.setBubbleSpec(BubbleSpec.FACE_BOTTOM);
                 x.setAlignment(Pos.CENTER);
@@ -253,7 +263,7 @@ public class ChatController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-                /* Drag and Drop */
+        /* Drag and Drop */
         borderPane.setOnMousePressed(event -> {
             xOffset = MainLauncher.getPrimaryStage().getX() - event.getScreenX();
             yOffset = MainLauncher.getPrimaryStage().getY() - event.getScreenY();
@@ -266,17 +276,16 @@ public class ChatController implements Initializable {
 
         });
 
-        borderPane.setOnMouseReleased(event -> {
-            borderPane.setCursor(Cursor.DEFAULT);
-        });
+        borderPane.setOnMouseReleased(event -> borderPane.setCursor(Cursor.DEFAULT));
 
-         statusComboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            try {
-                Listener.sendStatusUpdate(Status.valueOf(newValue.toUpperCase()));
-            } catch (IOException e) {
-                logger.error("Could not send the status update!", e);
-            }
-        });
+        statusComboBox.getSelectionModel().selectedItemProperty()
+                .addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
+                    try {
+                        Listener.sendStatusUpdate(Status.valueOf(newValue.toUpperCase()));
+                    } catch (IOException e) {
+                        logger.error("Could not send the status update!", e);
+                    }
+                });
 
         /* Added to prevent the enter from adding a new line to inputMessageBox */
         messageBox.addEventFilter(KeyEvent.KEY_PRESSED, ke -> {
@@ -295,13 +304,16 @@ public class ChatController implements Initializable {
     public void setImageLabel(String selectedPicture) {
         switch (selectedPicture) {
             case "Dominic":
-                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
+                this.userImageView
+                        .setImage(new Image(getClass().getClassLoader().getResource("images/dominic.png").toString()));
                 break;
             case "Sarah":
-                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/sarah.png").toString()));
+                this.userImageView
+                        .setImage(new Image(getClass().getClassLoader().getResource("images/sarah.png").toString()));
                 break;
             case "Default":
-                this.userImageView.setImage(new Image(getClass().getClassLoader().getResource("images/default.png").toString()));
+                this.userImageView
+                        .setImage(new Image(getClass().getClassLoader().getResource("images/default.png").toString()));
                 break;
             default:
                 logger.warn("No image resource for '{}' available!", selectedPicture);
